4df92bed8023ddf4a7ad6f4cb95f26e8d537febd
mhikmal03 <hikmalkp@gmail.com>
2023-07-11 15:00:49 +0700

    mantap

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,13 +9,13 @@ const Home = () => {
         <Navbar/>
         <div className='font-inter'>
             <div className='py-[60px] max-sm:w-full flex items-center lg:w-full lg:h-[400px] rounded-[16px] bg-no-repeat bg-cover' style={{backgroundImage:`url(${BackgroundImage})`}}>
-                <div className='h-400 sm:ml-[72px]'>
-                    <h1 className='max-md:text-center text-white sm:text-[32px] sm:font-bold leading-[42px] sm:mb-7 '>Seek and Find<br/> your Ideal property</h1>
-                    <div className='flex flex-row items-center justify-start bg-white w-[512px] sm:pl-6 sm:gap-3 rounded-xl sm:mb-5'>
+                <div className='h-400 sm:ml-[72px] w-full'>
+                    <h1 className='max-md:text-center text-white text-[26px]  sm:text-[32px]  font-bold md:leading-[42px] mb-7 '>Seek and Find<br/> your Ideal property</h1>
+                    <div className='flex flex-row items-center justify-start bg-white w-full sm:pl-6 sm:gap-3 rounded-xl sm:mb-5'>
                         <button className='text-[24px] dark'>
                             <MdSearch/>
                         </button>
-                        <input className=' sm:py-[18px] w-[512px] text-[16px] outline-none bg-transparent' type="text" placeholder='Masukkan kata kunci atau Lokasi' />
+                        <input className='py-4 sm:py-[18px] w-full text-[16px] outline-none bg-transparent' type="text" placeholder='Masukkan kata kunci atau Lokasi' />
                     </div>
                     <div className='text-white'>
                         <p>Popular Search: </p>
