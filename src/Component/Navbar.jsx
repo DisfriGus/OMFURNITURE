0e6d979521e40dfea12f97d7cb2644bd40dae79e
Dio Siregar <dio.siregar03@gmail.com>
2023-07-14 22:20:35 +0700

    dio/navbar

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 const Navbar = ({logo, style}) => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(true)
   return (
       
       <div className={`font-inter flex items-center justify-between px-2 md:px-[80px] xl:px-[240px]  py-3 xl:py-[26px] max-lg:fixed max-lg:w-full   bg-white xl:bg-transparent ${style}`} >
         <NavLink to="/" className=''>
           <img className='w-[157px] h-[29px] ' src={logo} alt="" />
         </NavLink>
-        <div className='flex pl-7 gap-[20px] text-[16px] text-white font-normal max-xl:flex-col '>
+        <div className='flex pl-7 gap-[20px] text-[16px] text-white font-normal max-xl:flex-col max-xl:hidden '>
           <NavLink to='/Dijual'>Buy</NavLink>
           <NavLink to="/Disewa">Rent</NavLink>
           <NavLink to="/PropertyBaru">Sell</NavLink>
           <NavLink to="/DetailPage">Find Broker</NavLink>
         </div>
-        <div className={`md:hidden text-[18px] font-medium flex flex-col gap-10 pt-4 absolute top-12 duration-200 bg-white w-full h-[800px] z-20 items-center  ${open ? 'translate-x-[1000px]':'translate-x-0'}`}>
-            <NavLink to='/Article '>Article</NavLink>
-            <NavLink to='/Destination'>Destination</NavLink>
-            <NavLink to='/YourOrders'>Your Orders</NavLink>
+        <div className={`xl:hidden text-black text-[18px] font-medium flex flex-col gap-10  absolute top-[52px] duration-200 bg-white w-full h-[2000px] z-20 items-center  ${open ? 'translate-x-[2000px]':'translate-x-[-8px] md:translate-x-[-80px]'}`}>
+          <NavLink to='/Dijual'>Buy</NavLink>
+          <NavLink to="/Disewa">Rent</NavLink>
+          <NavLink to="/PropertyBaru">Sell</NavLink>
+          <NavLink to="/DetailPage">Find Broker</NavLink>
+        </div>
+        <div className='flex gap-3 font-normal max-xl:hidden text-white items-center'>
+          <NavLink className={`py-2 px-6  `}>Login</NavLink>
+          <NavLink className={`py-2 px-6 bg-[#1659E6] rounded-md `}>Sign Up</NavLink>
         </div>
         <div className='xl:hidden'>
           <button onClick={()=>{
