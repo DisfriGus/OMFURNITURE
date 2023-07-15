2dcf90f759148ce9691695c29ca0cb6a3cf4b4ca
Dio Siregar <dio.siregar03@gmail.com>
2023-07-16 02:01:01 +0700

    dio/detailPage

--- src/Pages/DetailPage.jsx
+++ src/Pages/DetailPage.jsx
@@ -2,7 +2,7 @@ import React, { useState,useEffect } from 'react'
 import Navbar from '../Component/Navbar'
 import { useNavigate, useParams } from 'react-router-dom'
 import Profile from '../Assets/kam-idris-_HqHX3LBN18-unsplash.jpg'
-
+import Logo from '../Assets/logo2.svg'
 const DetailPage = () => {
     const [place, setPlace] = useState(null)
     const {id} = useParams()
@@ -17,9 +17,9 @@ const DetailPage = () => {
     }
   return (
     <div>
-      <Navbar />
-      <div className='py-11 px-[240px] flex gap-11 items-center'>
-        <div className=" w-[720px]  max-sm:py-10 max-sm:px-6 ">
+      <Navbar logo={Logo} />
+      <div className='py-11 lg:px-[240px] flex max-lg:flex-col  gap-11 items-center'>
+        <div className="  lg:w-[720px]  max-sm:py-10 max-sm:px-6 ">
             <div className="flex flex-row w-full items-center gap-2 mb-4">
             <button onClick={() => navigate(-1)} className="bg-[#F7F8FB] w-[50px] h-[50px] flex items-center justify-center rounded-full">
                 <h5 className="subHeading">Back</h5>
@@ -32,20 +32,20 @@ const DetailPage = () => {
             <div className='flex flex-col gap-[29px]'>
                 <img className='w-[720px] h-[400px]' src={Profile} alt="" />
                 <div>
-                    <img className='w-[224px] h-[120px]' src={Profile} alt="" />
+                    <img className='w-[100px] h-[100px] lg:w-[224px] lg:h-[120px] object-cover object-center' src={Profile} alt="" />
                 </div>
             </div>
         </div>
-        <div >
+        <div className='max-lg:w-full max-lg:px-4' >
             <h1 className='font-satoshi text-[36px] font-bold'>Nama Barang</h1>
             <p className='mb-[50px]'>Description</p>
             <h3 className='text-[24px]'><span className='text-[#1659E6] font-bold'>$</span> Harga</h3>
             <h3 className='mb-[50px]'>teuing teu apal</h3>
             <div className='flex mb-[26px]'>
-                <div className='flex bg-[#F2F2F2] w-fit gap-[18px] rounded-3xl text-[16px]'>
-                    <button onClick={pengurangan} className='hover:bg-black pl-[18px] py-3 rounded-bl-3xl rounded-tl-3xl '>-</button>
+                <div className='flex bg-[#F2F2F2] w-fit gap-[9px] rounded-3xl text-[16px]'>
+                    <button onClick={pengurangan} className='hover:hover:bg-slate-200 flex pl-[18px] pr-[9px] py-3 rounded-bl-3xl rounded-tl-3xl '>-</button>
                     <p className='py-3'>{count}</p>
-                    <button onClick={()=>setCount(count + 1)} className='hover:bg-black pr-[18px] py-3 rounded-br-3xl rounded-tr-3xl '>+</button>
+                    <button onClick={()=>setCount(count + 1)} className='hover:bg-gray-200 pr-[18px] pl-[9px] py-3 rounded-br-3xl rounded-tr-3xl '>+</button>
                 </div>
                 <div>
                     <p>Only ... left Dont mess it</p>
